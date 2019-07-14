f78b9283895480e7ceb84f1dd2749f6aa8164273
Ilya Zhuravlev <ichirozcom@gmail.com>
2019-07-14 19:19:07 +0300

    feat(app): Vue skeleton

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Hello {{name}}</h1>
+</template>
+<script>
+  export default {
+    name: 'App',
+    data: () => {
+      return {
+        name: 'test',
+      }
+    }
+  }
+</script>
